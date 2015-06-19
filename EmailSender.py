67f56097df713d90d072a3d36cd756d1797d9674
okeonwuka <okeonwuka@gmail.com>
2015-06-19 14:07:55 -0700

    First iteration of emailsender

---
+++ EmailSender.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+import smtplib
+
+content = 'Welcome to spotfire'
+
+mail = smtplib.SMTP('smtp.gmail.com',465)
+
+mail.ehlo()
+
+mail.starttls()
+
+mail.login('okeonwuka@gmail.com','4Evarelentle$$')
+
+mail.sendmail('okeonwuka@gmail.com','okeonwuka@yahoo.com', content)
+
+mail.close()
+

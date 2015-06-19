8c73d85e38ea9d80013f5aa7f1db4c7b913c3b37
okeonwuka <okeonwuka@gmail.com>
2015-06-19 16:30:18 -0700

    B B B B B B B B B B B A A A A A A A A A A A A A A A A A A A A A B B B B B B B B B B B B B B B B B B B B B please Commit my chairman
    A
    A
    A
    A

---
+++ EmailSender2.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import smtplib
+server = smtplib.SMTP('smtp.gmail.com', 465)
+
+#Next, log in to the server
+server.login("okeonwuka@gmail.com", "Iamrelente$$")
+
+#Send the mail
+msg = "\nHello!" # The /n separates the message from the headers
+server.sendmail("okeonwuka@gmail.com", "okeonwuka@gmail.com", msg)
+server.quit()
---
+++ EmailSender4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import smtplib
+
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.ehlo()
+session.starttls()
+session.login('4evarelentless@gmail.com','whysoserious?')
+headers = "\r\n".join(["from: " + '4evarelentless@gmail.com',
+                       "subject: " + "test",
+                       "to: " + 'okeonwuka@gmail.com',
+                       "mime-version: 1.0",
+                       "content-type: text/html"])
+
+# body_of_email can be plaintext or html!                    
+content = headers + "\r\n\r\n" + "body_of_email"
+session.sendmail('4evarelentless@gmail.com', 'okeonwuka@gmail.com', content)
